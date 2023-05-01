067ec38e9582b06e96c9e728d6ad9aa5a2734552
Askhat Bagaviyev <altair5555@gmail.com>
2023-05-01 17:59:16 +0600

    init

---
+++ src/main.js
@@ -0,0 +1,81 @@
+import { Telegraf, session } from "telegraf";
+import { message } from "telegraf/filters";
+import { code } from "telegraf/format";
+import config from "config";
+import { ogg } from "./ogg.js";
+import { openai } from "./openai.js";
+
+console.log(config.get("TEST_ENV"));
+
+const INITIAL_SESSION = {
+  messages: [],
+};
+
+const bot = new Telegraf(config.get("TELEGRAM_TOKEN"));
+
+bot.use(session());
+
+bot.command("new", async (ctx) => {
+  ctx.session = INITIAL_SESSION;
+  await ctx.reply("Waiting your request!");
+});
+
+bot.command("start", async (ctx) => {
+  ctx.session = INITIAL_SESSION;
+  await ctx.reply("Waiting your request!");
+});
+
+bot.on(message("voice"), async (ctx) => {
+  ctx.session ??= INITIAL_SESSION;
+  try {
+    ctx.reply(code("Message accepted. Waiting response from server."));
+    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+    const userId = String(ctx.message.from.id);
+    const oggPath = await ogg.create(link.href, userId);
+    const mp3Path = await ogg.toMp3(oggPath, userId);
+
+    const text = await openai.transcription(mp3Path);
+    ctx.reply(code(`Your request: ${text}`));
+
+    ctx.session.messages.push({ role: openai.roles.USER, content: text });
+    const response = await openai.chat(ctx.session.messages);
+    ctx.session.messages.push({
+      role: openai.roles.ASSISTANT,
+      content: response.content,
+    });
+
+    await ctx.reply(response.content);
+  } catch (error) {
+    console.log("Error on voice message: ", error.message);
+  }
+});
+
+bot.on(message("text"), async (ctx) => {
+  ctx.session ??= INITIAL_SESSION;
+  try {
+    ctx.reply(code("Message accepted. Waiting response from server."));
+
+    ctx.session.messages.push({
+      role: openai.roles.USER,
+      content: ctx.message.text,
+    });
+    const response = await openai.chat(ctx.session.messages);
+    ctx.session.messages.push({
+      role: openai.roles.ASSISTANT,
+      content: response.content,
+    });
+
+    await ctx.reply(response.content);
+  } catch (error) {
+    console.log("Error on voice message: ", error.message);
+  }
+});
+
+bot.command("start", async (ctx) => {
+  await ctx.reply(JSON.stringify(ctx.message, null, 2));
+});
+
+bot.launch();
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
